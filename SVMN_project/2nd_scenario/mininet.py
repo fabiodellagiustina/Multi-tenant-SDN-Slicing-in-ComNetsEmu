83fa97d2c2f8cd8d3547847edc07b403b6afa4a0
Davide Gagliardi <davide@webgraphical.it>
2020-07-13 22:54:53 +0200

    echo server for topo2

--- SVMN_project/2nd_scenario/mininet.py
+++ SVMN_project/2nd_scenario/mininet.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+import os
+
+from comnetsemu.cli import CLI, spawnXtermDocker
+from comnetsemu.net import Containernet, VNFManager
+from mininet.link import TCLink
+from mininet.log import info, setLogLevel
+from mininet.node import Controller
 from mininet.topo import Topo
 
 class FVTopo(Topo):
@@ -7,6 +14,9 @@ class FVTopo(Topo):
     def __init__(self):
         # Initialize topology
         Topo.__init__(self)
+
+        net = Containernet(controller=Controller, link=TCLink, xterms=False)
+        mgr = VNFManager(net)
 
         # Create template host, switch, and link
         hconfig = {'inNamespace':True}
@@ -21,7 +31,7 @@ class FVTopo(Topo):
 
         # Create host nodes
         for i in range(6):
-            self.addHost('h%d' % (i+1), **hconfig)
+            self.addDockerHost('h%d' % (i+1), dimage="dev_test", ip="10.0.0.%d" % (i+1), docker_args={"hostname": "h%d" % (i+1)} **hconfig)
 
         # Add switch links
         self.addLink('s1', 's3', **http_link_config)
@@ -40,5 +50,27 @@ class FVTopo(Topo):
         self.addLink('h4', 's6', **host_link_config)
         self.addLink('h5', 's7', **host_link_config)
         self.addLink('h6', 's7', **host_link_config)
+
+        srv1 = self.addContainer("srv1", "h1", "dev_test", "bash", docker_args={})
+        srv2 = self.addContainer("srv2", "h2", "dev_test", "bash", docker_args={})
+        srv3 = self.addContainer("srv3", "h3", "dev_test", "bash", docker_args={})
+        srv4 = self.addContainer("srv4", "h4", "echo_server", "python /home/server.py", docker_args={})
+        srv5 = self.addContainer("srv5", "h5", "echo_server", "python /home/server.py", docker_args={})
+        srv6 = self.addContainer("srv6", "h6", "echo_server", "python /home/server.py", docker_args={})
+
+        #if not AUTOTEST_MODE:
+            # Cannot spawn xterm for srv1 since BASH is not installed in the image:
+            # echo_server.
+            #spawnXtermDocker("srv2")
+            #CLI(net)
+
+        self.removeContainer("srv1")
+        self.removeContainer("srv2")
+        self.removeContainer("srv3")
+        self.removeContainer("srv4")
+        self.removeContainer("srv5")
+        self.removeContainer("srv6")
+        #net.stop()
+        #mgr.stop()
 
 topos = { 'fvtopo': ( lambda: FVTopo() ) }
