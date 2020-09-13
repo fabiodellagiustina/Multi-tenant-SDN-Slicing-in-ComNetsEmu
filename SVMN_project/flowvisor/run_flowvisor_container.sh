903f699806fc13199b8715b310dfec9560c283ee
Fabio Della Giustina <44431164+fabiodellagiustina@users.noreply.github.com>
2020-09-13 14:44:58 +0300

    Update run_flowvisor_container.sh

--- SVMN_project/flowvisor/run_flowvisor_container.sh
+++ SVMN_project/flowvisor/run_flowvisor_container.sh
@@ -2,5 +2,7 @@
 #
 # About: Run FlowVisor docker image with host networking and interactive mode
 #
+# WARNING: This is an old script with an absolute path, run the command with the relative path instead as described in the README.
+#
 
 docker run -v /home/vagrant/comnetsemu/SVMN_project/flowvisor/slicing_scripts:/root/slicing_scripts -it --rm --network host flowvisor:latest /bin/bash
